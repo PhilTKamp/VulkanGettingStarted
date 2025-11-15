e6d25e51f59e0c58568ea6e7f64f3d4e56fc10e5
LylaT <lylatelkamp@gmail.com>
2025-11-14 18:34:07 -0800

    Initial setup and adding CreateInstance

---
+++ vulkanGettingStarted.cpp
@@ -0,0 +1,140 @@
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+const std::vector<char const *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        createInstance();
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
+
+    void createInstance()
+    {
+        constexpr vk::ApplicationInfo appInfo{
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = vk::ApiVersion14};
+
+        std::vector<char const *> requiredLayers;
+        if (enableValidationLayers)
+        {
+            requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+        }
+
+        auto layerProperties = context.enumerateInstanceLayerProperties();
+        if (std::ranges::any_of(requiredLayers, [&layerProperties](auto const &requiredLayer)
+                                { return std::ranges::none_of(layerProperties, [requiredLayer](auto const &layerProperty)
+                                                              { return strcmp(layerProperty.layerName, requiredLayer) == 0; }); }))
+        {
+            throw std::runtime_error("One or more required layers are not supported!");
+        }
+
+        auto requiredExtensions = getRequiredExtensions();
+
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (auto const &requiredExtension : requiredExtensions)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                                     [requiredExtension](auto const &extensionProperty)
+                                     { return strcmp(extensionProperty.extensionName, requiredExtension) == 0; }))
+            {
+                throw std::runtime_error("Required extension not supported: " + std::string(requiredExtension));
+            }
+        }
+        vk::InstanceCreateInfo createInfo{
+            .pApplicationInfo = &appInfo,
+            .enabledLayerCount = static_cast<uint32_t>(requiredLayers.size()),
+            .ppEnabledLayerNames = requiredLayers.data(),
+            .enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size()),
+            .ppEnabledExtensionNames = requiredExtensions.data()};
+
+        instance = vk::raii::Instance(context, createInfo);
+
+        auto extensionProps = context.enumerateInstanceExtensionProperties();
+
+        std::cout << "available extensions:\n";
+
+        for (const auto &extension : extensionProps)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+    }
+
+    void initWindow()
+    {
+    }
+    void initVulkan()
+    {
+    }
+
+    void mainLoop()
+    {
+    }
+
+    void cleanup()
+    {
+    }
+
+    std::vector<const char *> getRequiredExtensions()
+    {
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        if (enableValidationLayers)
+        {
+            extensions.push_back(vk::EXTDebugUtilsExtensionName);
+        }
+
+        return extensions;
+    }
+};
+
+int main()
+{
+    HelloTriangleApplication app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
