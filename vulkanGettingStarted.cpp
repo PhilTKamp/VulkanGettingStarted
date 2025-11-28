0d6306d88e81624fe158c3da216915f9fc1f2f10
LylaT <lylatelkamp@gmail.com>
2025-11-28 12:44:16 -0800

    Tweaks to discover reason for particle persistence.
    Particles from the initial frame continue to persist on the rendered image

--- vulkanGettingStarted.cpp
+++ vulkanGettingStarted.cpp
@@ -216,7 +216,9 @@ private:
         vk::ImageViewCreateInfo imageViewCreateInfo{
             .viewType = vk::ImageViewType::e2D,
             .format = swapChainSurfaceFormat.format,
-            .subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}};
+            .components = {vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity}, // CHECK: Unsure if this is necessary or if this is the default?
+            .subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1},
+        };
 
         for (auto image : swapChainImages)
         {
